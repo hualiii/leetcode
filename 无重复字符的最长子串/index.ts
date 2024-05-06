e8e46e3290714765c3780c6ac51c656cf6e00932
QHL <qiuhuali@fotric.cn>
2024-05-06 11:29:15 +0800

    init

---
+++ 无重复字符的最长子串/index.ts
@@ -0,0 +1,14 @@
+// link:https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+function lengthOfLongestSubstring(s: string): number {
+    const window = new Set();
+    let ans: number = 0;
+    for (let i = 0; i < s.length; i++) {
+        const c = s.charAt(i);
+        while (window.has(c)) {
+            window.delete(s.charAt(i - window.size));
+        }
+        window.add(c);
+        ans = Math.max(ans, window.size);
+    }
+    return ans;
+};
