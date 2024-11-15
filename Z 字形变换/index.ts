bab45a29a31595c883f734052469762e6f483746
QHL <qiuhuali@fotric.cn>
2024-11-15 16:02:10 +0800

    Z 字形变换

---
+++ Z 字形变换/index.ts
@@ -0,0 +1,27 @@
+function convert(s: string, numRows: number): string {
+    if(numRows<2){
+        return  s
+    }
+    const result: string[] = new Array(numRows).fill("");
+    let flag = true;
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (flag) {
+            result[count] += s[i]
+            count++
+            if (count === numRows) {
+                flag = false;
+                count = numRows - 2;
+            }
+        } else {
+            result[count] +=  s[i]
+            count--
+            if (count === -1) {
+                flag = true;
+                count = 1
+            }
+        }
+    }
+    return result?.join('')
+};
+
